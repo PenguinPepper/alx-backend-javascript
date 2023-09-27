a3187de5e14d5ff97570d7cc5db7267fecbb4b4b
Penguin Peps <112114400+PenguinPepper@users.noreply.github.com>
2023-09-28 01:01:06 +0200

    Have multiple try catch statements

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,8 +4,24 @@ import createUser from './utils.js';
 export async function asyncUploadUser() {
   try {
     const response_from_uploadPhoto_function = await uploadPhoto();
-    const response_from_createUser_function = await createUser();
+  } catch {
+    const catch_obj = { 
+      photo: null, \n
+      user: null,
+    };
 
+    return catch_obj;
+  }
+  try{
+    const response_from_createUser_function = await createUser();
+  } catch {
+    const catch_obj = { 
+      photo: null, \n
+      user: null,
+    };
+
+    return catch_obj;
+  }
     const new_obj = {
       photo: response_from_uploadPhoto_function,
       user: response_from_createUser_function,
