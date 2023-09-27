4343b765456707d9a4bc630be8d2a6e68149209d
Penguin Peps <112114400+PenguinPepper@users.noreply.github.com>
2023-09-27 23:40:51 +0200

    Append 3 handlers, one for resolved promises, another for rejected and one for each time a response is returned

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-
+  return new Promise((resolve, reject) => {
+    resolve({
+      status: 200,
+      body: 'sucess',
+    });
+    reject(Error());
+})
 }
+
+handleResponseFromAPI().then(() => {
+  console.log("Got a response from the API");
+})
