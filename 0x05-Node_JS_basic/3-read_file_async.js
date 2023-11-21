5e8df9f3c94d2aea1278018a32dc05ab90cfe67f
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-21 10:14:16 +0000

    Add script that attempts to read the database file asynchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,38 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const { parse } = require('csv-parse');
 
-function countStudents(path) {
+async function countStudents(path) {
   let data;
   try {
-    data = fs.readFile(path);
+    data = await fs.readFile(path);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 
-  const students = [];
-  const fields = {};
-  parse(data, { delimiter: ',', columns: true })
-    .on('data', (row) => {
-      students.push(row);
-      if (!fields[row.field]) {
-        fields[row.field] = [];
-      }
-      fields[row.field].push(row.firstname);
-    })
-    .on('end', () => {
-      console.log(`Number of students: ${students.length}`);
-      for (const field in fields) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-      }
-    });
+  return new Promise((resolve, reject) => {
+    const students = [];
+    const fields = {};
+    parse(data, { delimiter: ',', columns: true })
+      .on('data', (row) => {
+        students.push(row);
+        if (!fields[row.field]) {
+          fields[row.field] = [];
+        }
+        fields[row.field].push(row.firstname);
+      })
+      .on('end', () => {
+        console.log(`Number of students: ${students.length}`);
+        for (const field in fields) {
+          if (field) {
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+          }
+        }
+        resolve();
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
 }
 
 module.exports = countStudents;
