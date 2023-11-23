89b3cb9c8b2f12ed3314b8857579d60d8b199feb
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-23 07:19:48 +0000

    Add four more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,5 +6,23 @@ describe('Test rounding off', function() {
     answer = 1 + Math.round(3.7);
     func_ans = calculateNumber(1, 3.7);
     assert.deepStrictEqual(answer, func_ans);
-  })
+  });
+
+  it('Round off two decimals', function() {
+    answer = 2 + 4;
+    func_ans = calculateNumber(1.5, 3.7);
+    assert.deepStrictEqual(answer, func_ans);
+  });
+
+  it('Round off a negative decimal number', function() {
+    answer = -1 + 2;
+    func_ans = calculateNumber(-1.5, 2.1);
+    assert.deepStrictEqual(answer, func_ans);
+  });
+
+  it('Round off two negative decimal numbers', function() {
+   answer = -3 + (-3);
+   func_ans = calculateNumber(-3.2, -2.7);
+   assert.deepStrictEqual(answer, func_ans);
+  });
 })
