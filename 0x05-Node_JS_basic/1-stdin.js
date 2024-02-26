e910162889e0eb679f0de6a5cda2216bf2e218fd
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-02-26 20:02:58 +0000

    Edit file to pass linter

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdin.setEncoding('utf8');
 
-process.stdout.write(`Welcome to Holberton School, what is your name?\n`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
@@ -10,5 +10,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  console.log(`This important software is now closing`);
+  console.log('This important software is now closing');
 });
