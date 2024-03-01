5e7afdc004da5081af6aed72dc0da236f17c7734
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-03-01 21:18:47 +0000

    Remove newline in response

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,4 +1,4 @@
-const http = require('node:http');
+const http = require('http');
 
 
 const port = 1245;
@@ -7,7 +7,7 @@ const hostname = '127.0.0.1';
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello Holberton School!\n');
+  response.end('Hello Holberton School!');
 });
 
 server.listen(port, hostname, () => {
