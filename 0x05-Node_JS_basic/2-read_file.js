b023d4e110e27a4e552aee07892f54aea896bdab
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-03-01 11:25:56 +0000

    Edit file to meet eslint standards

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,32 +2,32 @@ const fs = require('fs');
 const { parse } = require('csv-parse');
 
 function countStudents(path) {
-    let data;
-    try {
-        data = fs.readFileSync;
-    } catch (error) {
-        throw new Error('Cannot load the database');
-    }
+  let data;
+  try {
+    data = fs.readFileSync(path);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 
-    const students = {};
-    const fields = {};
+  const students = {};
+  const fields = {};
 
-    parse(data, { delimter: ',', columns: true })
-        .on('data', (row) => {
-            students.push(row);
-            if (!fields[row.field]) {
-                fields[row.field] = [];
-            }
-            fields[row.field].push(row.student);
-        })
-        .on('end', () => {
-            console.log(`Number of students: ${students.length}`);
-            for (const field in fields) {
-                if (field) {
-                    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-                }
-            }
-        });
+  parse(data, { delimter: ',', columns: true })
+    .on('data', (row) => {
+      students.push(row);
+      if (!fields[row.field]) {
+        fields[row.field] = [];
+      }
+      fields[row.field].push(row.student);
+    })
+    .on('end', () => {
+      console.log(`Number of students: ${students.length}`);
+      for (const field in fields) {
+        if (field) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+      }
+    });
 }
 
 module.exports = countStudents;
