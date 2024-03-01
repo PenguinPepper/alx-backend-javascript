5bf2d8799677faf204226acea37241a066708fdd
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-03-01 12:30:56 +0000

    Edit function in / route

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -22,7 +22,9 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 const port = 1245;
 
-app.get('/', (request, response) => response.send('Hello Holberton School!'));
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!'
+});
 
 app.get('/students', (request, response) => {
   const students = countStudents(process.argv[2]);
