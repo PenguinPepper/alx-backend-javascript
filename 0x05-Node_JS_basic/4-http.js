cc551a3df09a9d0ae05348da60a2ea2f1aebbb89
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-03-01 21:22:55 +0000

    Change name of http.Server variable to app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,14 @@
 const http = require('http');
 
-
 const port = 1245;
 const hostname = '127.0.0.1';
 
-const server = http.createServer((request, response) => {
+const app = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/plain');
   response.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at ${hostname} on ${port}`);
 });
