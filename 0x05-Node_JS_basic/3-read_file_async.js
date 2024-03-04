e71fb1504cc07505135dbc81659cd46a3070213f
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-03-04 08:49:15 +0000

    Rewrite file so that it does implement csv-parse but used readline module and fs module

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,40 +1,56 @@
-const fs = require('fs').promises;
-const { parse } = require('csv-parse');
+const fs = require('fs');
+const readline = require('readline');
+
+async function parse(filePath) {
+  const fileStream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  const lines = [];
+
+  for await (const line of rl) {
+    if (line.trim() !== '') {
+      lines.push(line.split(',').map((value) => value.trim()));
+    }
+  }
+
+  const [header, ...rows] = lines;
+  return rows.map((row) => header.reduce((obj, key, i) => ({ ...obj, [key]: row[i] }), {}));
+}
 
 async function countStudents(path) {
-  let data;
+  let students;
   try {
-    data = await fs.readFile(path);
+    students = await parse(path);
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 
   return new Promise((resolve, reject) => {
-    const students = [];
-    const fields = {};
+    try {
+      const fields = {};
 
-    parse(data, {
-      delimter: ',', columns: true, relax_column_count: true, skip_empty_lines: true,
-    })
-      .on('data', (row) => {
-        students.push(row);
-        if (!fields[row.field]) {
-          fields[row.field] = [];
+      students.forEach((student) => {
+        if (!fields[student.field]) {
+          fields[student.field] = [];
         }
-        fields[row.field].push(row.firstname);
-      })
-      .on('end', () => {
-        console.log(`Number of students: ${students.length}`);
-        for (const field in fields) {
-          if (field) {
-            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-          }
-        }
-        resolve();
-      })
-      .on('error', (err) => {
-        reject(err);
+        fields[student.field].push(student.firstname);
       });
+
+      console.log(`Number of students: ${students.length}`);
+      for (const field in fields) {
+        if (field) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+      }
+      resolve();
+    } catch (error) {
+      console.error(error);
+      reject(error);
+    }
   });
 }
 
