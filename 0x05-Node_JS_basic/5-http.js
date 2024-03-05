0b2ec970686d3cb41d69933fc22f0e1163025274
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-03-05 10:27:26 +0000

    Write the function for countStudents in the same file as the http request

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,82 @@
-const fs = require('fs').promises;
-const countStudents = require('./3-read_file_async');
+const readPromise = require('fs').promises;
+const fs = require('fs');
+const readline = require('readline');
 const http = require('http');
+
+async function parse(filePath) {
+
+  try {
+    await readPromise.access(filePath, fs.constants.F_OK);
+  }  catch (error) {
+    throw new Error('Cannot load database');
+  }
+
+  const fileStream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  const lines = [];
+    return new Promise((resolve, reject) => {
+      rl.on('line', (line) => {
+        if (line.trim() !== '') {
+          lines.push(line.split(',').map((value) => value.trim()));
+        }
+      });
+
+      rl.on('close', () => {
+        const [header, ...rows] = lines;
+        const result = [];
+        rows.forEach((row) => {
+          const obj = {};
+          for (let i = 0; i < header.length; i++) {
+            obj[header[i]] = row[i];
+          }
+          result.push(obj);
+        });
+        resolve(result);
+      });
+
+      rl.on('error', (error) => {
+        reject(error);
+      });
+    });
+  }
+
+async function countStudents(path) {
+  let students;
+  try {
+    students = await parse(path);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      const fields = {};
+
+      students.forEach((student) => {
+        if (!fields[student.field]) {
+          fields[student.field] = [];
+        }
+        fields[student.field].push(student.firstname);
+      });
+
+      let output = `Number of students: ${students.length}`;
+      for (const field in fields) {
+        if (field) {
+          output += `\n Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+        }
+      }
+      resolve(output);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -20,7 +96,7 @@ const app = http.createServer(async (request, response) => {
       } catch (error) {
         console.log(error);
         response.statusCode = 503;
-        response.end('Cannot load the database');
+        response.end(`This is the list of our students\n${error.message}`);
       }
       break;
     default:
