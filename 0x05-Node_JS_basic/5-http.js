53693f42ab74db3de4a5a24c9398f4c7221c4dc9
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-03-05 10:06:40 +0000

    Import file with countStudents function instead of rewriting it

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,46 +1,9 @@
 const fs = require('fs').promises;
-const { parse } = require('csv-parse');
+const countStudents = require('./3-read_file_async');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
-
-async function countStudents(path) {
-  let data;
-  try {
-    data = await fs.readFile(path);
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-
-  return new Promise((resolve, reject) => {
-    const students = [];
-    const fields = {};
-
-    parse(data, {
-      delimter: ',', columns: true, relax_column_count: true, skip_empty_lines: true,
-    })
-      .on('data', (row) => {
-        students.push(row);
-        if (!fields[row.field]) {
-          fields[row.field] = [];
-        }
-        fields[row.field].push(row.firstname);
-      })
-      .on('end', () => {
-        let output = `Number of students: ${students.length}`;
-        for (const field in fields) {
-          if (field) {
-            output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
-          }
-        }
-        resolve(output);
-      })
-      .on('error', (err) => {
-        reject(err);
-      });
-  });
-}
 
 const app = http.createServer(async (request, response) => {
   response.setHeader('Content-Type', 'text/plain');
